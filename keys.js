2b078c6b77ad5d8d0c4eeb09daa2d55f7a391864
TobseNatoR <42202420+TobseNatoR@users.noreply.github.com>
2019-07-16 16:44:14 +0200

    v.1.0.0

--- keys.js
+++ keys.js
@@ -1,51 +1,70 @@
 //Taste M
-mp.keys.bind(0x4D, true, function () {
+mp.keys.bind(0x4D, true, function ()
+{
+    if (mp.gui.cursor.visible) return;
     mp.events.callRemote('AutoAnAus');
 });
 
 //Taste Enter
-mp.keys.bind(0x0D, true, function () {
+mp.keys.bind(0x0D, true, function ()
+{
+    if (mp.gui.cursor.visible) return;
     mp.events.callRemote('EnterCheck');
 });
 
 //Taste E
-mp.keys.bind(0x45, true, function () {
+mp.keys.bind(0x45, true, function ()
+{
+    if (mp.gui.cursor.visible) return;
     mp.events.callRemote('ECheck');
 });
 
 //Taste K
-mp.keys.bind(0x4B, true, function () {
+mp.keys.bind(0x4B, true, function ()
+{
+    if (mp.gui.cursor.visible) return;
     mp.events.callRemote('KCheck');
 });
 
 //Taste B
-mp.keys.bind(0x42, true, function () {
+mp.keys.bind(0x42, true, function ()
+{
+    if (mp.gui.cursor.visible) return;
     mp.events.callRemote('BCheck');
 });
 
 //Taste F2
-mp.keys.bind(0x71, true, function () {
+mp.keys.bind(0x71, true, function ()
+{
+    if (mp.gui.cursor.visible) return;
     mp.events.callRemote('F2Check');
 });
 
 //Taste F3
-mp.keys.bind(0x71, true, function () {
+mp.keys.bind(0x71, true, function ()
+{
+    if (mp.gui.cursor.visible) return;
     mp.events.callRemote('Fahrzeug_Privat_Abschliessen');
 });
 
 //Taste F8
-mp.keys.bind(0x77, true, function () {
+mp.keys.bind(0x77, true, function ()
+{
+    if (mp.gui.cursor.visible) return;
     mp.events.callRemote('IBerry');
 });
 
 //Taste N
 mp.keys.bind(0x4E, true, function ()
 {
+    if (mp.gui.cursor.visible) return;
     mp.events.callRemote('NCheck');
 });
 
 //Taste L
-mp.keys.bind(0x4C, true, function () {
+mp.keys.bind(0x4C, true, function ()
+{
+    if (mp.gui.cursor.visible) return;
     let localVeh = mp.players.local.vehicle;
 	if (localVeh) 
 	{
@@ -64,7 +83,9 @@ mp.keys.bind(0x4C, true, function () {
 });
 
 //Taste Y
-mp.events.add('render', () => {
+mp.events.add('render', () =>
+{
+    if (mp.gui.cursor.visible) return;
     if (mp.keys.isUp(89) === true) 
 	{
         mp.events.callRemote('ScoreboardHiden');
