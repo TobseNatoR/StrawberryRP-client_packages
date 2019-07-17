1d97b0f8ab8e79999318cd9bae4ce3c38c22ba90
Toby Gallenkamp <gallenkamp.toby@web.de>
2019-07-17 20:44:42 +0200

    v.1.0.0

--- Server/Tacho/index.js
+++ Server/Tacho/index.js
@@ -9,61 +9,34 @@ mp.events.add('TachoUpdaten', (Kilometerstand, Sprit, Tank) => {
     km = Kilometerstand;
 	gas = Sprit;
     volumen = Tank;
-
-    if (player.vehicle && player.vehicle.getPedInSeat(-1) === player.handle) // Check if player is in vehicle and is driver
-    {
-        if (mp.players.local.vehicle) 
-        {
-            mp.game.audio.setRadioToStationName("OFF");
-            mp.game.audio.setUserRadioControlEnabled(false);
-        }
-        if (showed === false) // Check if speedo is already showed
-        {
-            speedo.execute("showSpeedo();");
-            showed = true;
-        }
-        /*Get vehicle infos*/
-        let vel = player.vehicle.getSpeed() * 3.6;
-        let rpm = player.vehicle.rpm * 1000;
-
-        speedo.execute(`update(${vel}, ${rpm}, ${gas}, ${km}, ${volumen});`); // Send data do CEF
-    }
-    else
-    {
-        if (showed)
-        {
-            speedo.execute("hideSpeedo();");
-            showed = false;
-        }
-    }
 });
 
-//mp.events.add('render', () =>
-//{
-//	if (player.vehicle && player.vehicle.getPedInSeat(-1) === player.handle) // Check if player is in vehicle and is driver
-//	{
-//		if (mp.players.local.vehicle) 
-//		{
-//     ﻿   	mp.game.audio.setRadioToStationName("OFF");
-//			mp.game.audio.setUserRadioControlEnabled(false);
-//		}
-//		if(showed === false) // Check if speedo is already showed
-//		{
-//			speedo.execute("showSpeedo();");
-//			showed = true;
-//		}
-//		/*Get vehicle infos*/
-//		let vel = player.vehicle.getSpeed() * 3.6;  	
-//		let rpm = player.vehicle.rpm * 1000; 			
+mp.events.add('render', () =>
+{
+	if (player.vehicle && player.vehicle.getPedInSeat(-1) === player.handle) // Check if player is in vehicle and is driver
+	{
+		if (mp.players.local.vehicle) 
+		{
+     ﻿   	mp.game.audio.setRadioToStationName("OFF");
+			mp.game.audio.setUserRadioControlEnabled(false);
+		}
+		if(showed === false) // Check if speedo is already showed
+		{
+			speedo.execute("showSpeedo();");
+			showed = true;
+		}
+		/*Get vehicle infos*/
+		let vel = player.vehicle.getSpeed() * 3.6;  	
+		let rpm = player.vehicle.rpm * 1000; 			
 		
-//		speedo.execute(`update(${vel}, ${rpm}, ${gas}, ${km}, ${volumen});`); // Send data do CEF
-//	}
-//	else
-//	{
-//		if(showed)
-//		{
-//			speedo.execute("hideSpeedo();");
-//			showed = false;
-//		}
-//	}
-//});
+		speedo.execute(`update(${vel}, ${rpm}, ${gas}, ${km}, ${volumen});`); // Send data do CEF
+	}
+	else
+	{
+		if(showed)
+		{
+			speedo.execute("hideSpeedo();");
+			showed = false;
+		}
+	}
+});
