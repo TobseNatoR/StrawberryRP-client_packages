fb2811335e05ddd5d7ba74450c73da474a9263df
TobseNatoR <42202420+TobseNatoR@users.noreply.github.com>
2019-07-18 15:54:11 +0200

    v.1.0.0

--- peds.js
+++ peds.js
@@ -1,8 +1,19 @@
-//Feste Bots
+﻿//Feste Bots
 let Stadthalle = mp.peds.new(mp.game.joaat('ig_mrs_thornhill'), new mp.Vector3(329.862, -1571.43, 30.3066), 319.494, (streamPed) => { streamPed.setAlpha(255); }, 0);
 let Arbeitsamt = mp.peds.new(mp.game.joaat('cs_nigel'), new mp.Vector3(245.231, -1605.58, 25.5576), 317.164, (streamPed) => { streamPed.setAlpha(255); }, 0);
 let Einreise = mp.peds.new(mp.game.joaat('a_m_m_business_01'), new mp.Vector3(815.822, -3001.06, 6.02094), 181.778, (streamPed) => { streamPed.setAlpha(255); }, 0);
 let Helmut = mp.peds.new(mp.game.joaat('ig_drfriedlander'), new mp.Vector3(793.214, -3021.96, 6.02094), 315.372, (streamPed) => { streamPed.setAlpha(255); }, 0);
+let Mannimblumenfeld = mp.peds.new(mp.game.joaat('ig_drfriedlander'), new mp.Vector3(0.0, 0.0, 0.0), 0.0, (streamPed) => { streamPed.setAlpha(255); }, 0);
+
+//Ped Animationen
+mp.game.streaming.requestAnimDict("switch@michael@sleep_in_car﻿");
+Mannimblumenfeld.taskPlayAnim("switch@michael@sleep_in_car﻿", "sleep_in_car_michael_facial", 1.0, -1.0, -1, 1, 1, true, true, true);
+
+mp.events.addCommand('botanim', (player) =>
+{
+    mp.game.streaming.requestAnimDict("switch@michael@sleep_in_car﻿");
+    Mannimblumenfeld.taskPlayAnim("switch@michael@sleep_in_car﻿", "sleep_in_car_michael_facial", 1.0, -1.0, -1, 1, 1, true, true, true);
+});
 
 //Dynamische Bots
 mp.events.add('boterstellen', (Name, posX, posY, posZ, RotZ) =>
