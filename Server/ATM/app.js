fbae5c78acf0f984da0132272fd2ad32740c2c2b
Max <m.hohnstock@gmai.com>
2019-07-22 00:43:43 +0200

    ATM Update
    Leichter Hauch von einem Gui. Popup für das Einzahlen fertig. Toast der beim Einzahlen Triggert wird noch gefixt

---
+++ Server/ATM/app.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function TriggerToast() {
+    document.getElementById("ToastForm").style.display = "block";
+    document.getElementById("EinzahlenForm").style.display = "none";
+}
+
+function openEinzahlen() {
+    document.getElementById("EinzahlenForm").style.display = "block";
+}
+
+function CloseEinzahlen() {
+    document.getElementById("EinzahlenForm").style.display = "none";
+}
