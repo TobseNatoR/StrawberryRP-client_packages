5d93c7062dde216291dbdc20ae9be398e21ed9b3
Max <m.hohnstock@gmai.com>
2019-07-22 14:23:40 +0200

    UPDATE

--- Server/ATM/app.js
+++ Server/ATM/app.js
@@ -4,39 +4,26 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function TriggerToast() {
     document.getElementById("ToastForm").style.display = "block";
     document.getElementById("EinzahlenForm").style.display = "none";
 }
 
+function TriggerToast2() {
+    document.getElementById("ToastForm2").style.display = "block";
+    document.getElementById("AuszahlForm").style.display = "none";
+}
+
 function openEinzahlen() {
     document.getElementById("EinzahlenForm").style.display = "block";
+}
+function openAuszahlen() {
+    document.getElementById("AuszahlenForm").style.display = "block";
 }
 
 function CloseEinzahlen() {
     document.getElementById("EinzahlenForm").style.display = "none";
+}
+function CloseAuszahlen() {
+    document.getElementById("AuszahlenForm").style.display = "none";
 }
