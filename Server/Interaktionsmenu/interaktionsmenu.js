8f2543b7a89110e958f3b1fd17b9b0eeda1fb8b9
Toby Gallenkamp <gallenkamp.toby@web.de>
2019-07-23 17:25:19 +0200

    interaktionsmenu

--- Server/Interaktionsmenu/interaktionsmenu.js
+++ Server/Interaktionsmenu/interaktionsmenu.js
@@ -1,45 +1,82 @@
+//Globals
+let Type = 0;
+
+function Art(Id)
+{
+	if(Id == 1)
+	{
+		for (var i = 1; i <= 6; i++) 
+		{
+			var x = document.getElementById("int" + i);
+			x.style.display = "none";
+		}
+	}
+	else if(Id == 2)
+	{
+		for (var i = 7; i <= 12; i++) 
+		{
+			var x = document.getElementById("int" + i);
+			x.style.display = "none";
+		}
+	}
+}
+
 function getid(obj) 
 {
 	let text = null;
+	let selected = 0;
 
-	//Interaktionsmenü ohne Auto
+	var x = document.getElementById(obj.id);
+
 	switch (obj.id) 
 	{
 		case "int1":
 			text = "Handeln";
+			selected = 1;
 			break;
 		case "int2":
 			text = "Personalausweis zeigen";
+			selected = 2;
 			break;
 		case "int3":
-			text = "Überfallen";
+			text = "Fahrerlaubnis zeigen";
+			selected = 3;
 			break;
 		case "int4":
-			text = "Test";
+			text = unescape("%DCberfallen");
+			selected = 4;
 			break;
 		case "int5":
 			text = "Test";
+			selected = 5;
 			break;
 		case "int6":
-			text = "Test";
+			text = "Motor";
+			selected = 6;
 			break;
 		case "int7":
-			text = "Test";
+			text = "Motor";
+			selected = 7;
 			break;
 		case "int8":
-			text = "Test";
+			text = unescape("Auf/Abschlie%DFen");
+			selected = 8;
 			break;
 		case "int9":
-			text = "Test";
+			text = "Kofferraum";
+			selected = 9;
 			break;
 		case "int10":
-			text = "Test";
+			text = "Motorhaube";
+			selected = 10;
 			break;
 		case "int11":
 			text = "Test";
+			selected = 11;
 			break;
 		case "int12":
 			text = "Test";
+			selected = 12;
 			break;
 		default:
 			text = "Bla"
@@ -47,4 +84,12 @@ function getid(obj)
 		
 	}
 	document.getElementById("itemdesc").innerHTML = text;
+}
+
+function encode_utf8(s) {
+  return unescape(encodeURIComponent(s));
+}
+
+function decode_utf8(s) {
+  return decodeURIComponent(escape(s));
 }
